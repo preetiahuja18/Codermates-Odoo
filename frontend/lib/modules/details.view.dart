26d37dcedc4d7f5c503d2ecd46466dd9327851c5
Ritesh <riteshsaini331@gmail.com>
2025-07-12 16:33:43 +0530

    Detail Screen Init

--- frontend/lib/modules/details.view.dart
+++ frontend/lib/modules/details.view.dart
@@ -1,39 +1,88 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:skill_swap/core/model/user.model.dart';
-import 'package:skill_swap/core/utils/navigation.dart';
+import 'package:skill_swap/core/utils/style.dart';
 import 'package:skill_swap/core/widget/button/filled.widget.dart';
 import 'package:skill_swap/core/widget/layout/body.layout.dart';
-import 'package:skill_swap/modules/request/view.dart';
+import 'package:skill_swap/modules/home/view.dart';
 
 class UserDetails extends StatefulWidget {
-
   final AxUserModel userModel;
 
-  const UserDetails({
-    super.key,
-    required this.userModel
-  });
+  const UserDetails({super.key, required this.userModel});
 
   @override
   State<UserDetails> createState() => _UserDetailsState();
 }
 
 class _UserDetailsState extends State<UserDetails> {
-
   @override
   Widget build(BuildContext context) {
-    return BodyLayout(
-      body: Column(
-        children: [
+    final int? roundRating = double.tryParse(
+      widget.userModel.rating ?? "",
+    )?.round();
+    String ratingStar = "";
+    if (roundRating != null) {
+      for (int i = 0; i < roundRating; i++) {
+        ratingStar = "$ratingStar ⭐";
+      }
+    }
 
-        ],
+    return BodyLayout(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 25.0, width: double.infinity),
+                CircleAvatar(
+                  backgroundImage: Image.network(
+                    "https://images.unsplash.com/photo-1654110455429-cf322b40a906?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww",
+                  ).image,
+                  radius: 90,
+                ),
+                if (widget.userModel.rating != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: Text(
+                      widget.userModel.rating!,
+                      style: AxStyle.secondaryTextStyle.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                if (ratingStar.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Text(ratingStar),
+                  ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(
+                    widget.userModel.name!,
+                    style: AxStyle.secondaryTextStyle.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            skillWidget(
+              title: "Skils Offerd",
+              skills: widget.userModel.skillsOffered,
+            ),
+            const SizedBox(height: 12.0),
+            skillWidget(
+              title: "Skils Want",
+              skills: widget.userModel.skillsOffered,
+            ),
+          ],
+        ),
       ),
-      bottomWidget: AxFilledButton(
-        label: "Request",
-        onPressed: () {
-          naviagteTo(context: context, widget: RequestSwap(userModel: widget.userModel));
-        }
-      ),
+      bottomWidget: AxFilledButton(label: "Request", onPressed: () {}),
     );
   }
 }
--- frontend/lib/modules/home/view.dart
+++ frontend/lib/modules/home/view.dart
@@ -12,6 +12,7 @@ import 'package:skill_swap/core/widget/components/input.dart';
 import 'package:skill_swap/core/widget/layout/body.layout.dart';
 import 'package:skill_swap/modules/details.view.dart';
 import 'package:skill_swap/modules/home/provider.dart';
+import 'package:skill_swap/modules/request/view.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -83,23 +84,23 @@ class _HomeState extends State<Home> {
                 provider.isApiCalling
                     ? Center(child: CircularProgressIndicator())
                     : Expanded(
-                      child : SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Column(
-                              children: getProfleTiles(users: provider.users)
-                            ),
-                            const SizedBox(height: 20.0,),
-                            PaginationWidget(
-                              currentPage: 2,
-                              totalPages: 10, 
-                              onPageChanged: (int i) {}
-                              )
-                          ],
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Column(
+                                children: getProfleTiles(users: provider.users),
+                              ),
+                              const SizedBox(height: 20.0),
+                              PaginationWidget(
+                                currentPage: 2,
+                                totalPages: 10,
+                                onPageChanged: (int i) {},
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
               ],
             ),
           ),
@@ -109,135 +110,150 @@ class _HomeState extends State<Home> {
   }
 
   List<Widget> getProfleTiles({required List<AxUserModel> users}) {
-    List<Widget>  children = [];
+    List<Widget> children = [];
 
-    for(AxUserModel user in users) {
-      if(children.isNotEmpty) {
-        children.add(const  SizedBox(height: 16.0));
+    for (AxUserModel user in users) {
+      if (children.isNotEmpty) {
+        children.add(const SizedBox(height: 16.0));
       }
 
       final int? roundRating = double.tryParse(user.rating ?? "")?.round();
       String ratingStar = "";
-      if(roundRating != null) {
-          for(int i =0; i < roundRating;i ++) {
-            ratingStar = "$ratingStar ⭐";
-          }
+      if (roundRating != null) {
+        for (int i = 0; i < roundRating; i++) {
+          ratingStar = "$ratingStar ⭐";
+        }
       }
 
-
       children.add(
-        Container(
-          decoration: AxStyle.cardDecoration.copyWith(
-            boxShadow: []
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: Image.network(
-                            "https://images.unsplash.com/photo-1654110455429-cf322b40a906?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww"                  
-                          ).image ,
-                          radius: 60,
-                        ),
-                        if(user.rating != null)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 5.0),
-                            child: Text(user.rating!,
-                            style: AxStyle.secondaryTextStyle.copyWith(
-                              fontWeight: FontWeight.bold
-                            ),),
-                          ),
-                        if(ratingStar.isNotEmpty)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 5.0),
-                            child: Text(ratingStar),
-                          )
-                      
-                    
-                      ],
-                    ),
-                    const SizedBox(width: 16.0),
-                    Flexible(
-                      child: Column(
+        InkWell(
+          onTap: () {
+            naviagteTo(
+              context: context,
+              widget: UserDetails(userModel: user),
+            );
+          },
+          child: Container(
+            decoration: AxStyle.cardDecoration.copyWith(boxShadow: []),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                child: Text(
-                                  user.name!,
-                                  style: AxStyle.secondaryTextStyle.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16.0
-                                  ),
+                          CircleAvatar(
+                            backgroundImage: Image.network(
+                              "https://images.unsplash.com/photo-1654110455429-cf322b40a906?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww",
+                            ).image,
+                            radius: 60,
+                          ),
+                          if (user.rating != null)
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child: Text(
+                                user.rating!,
+                                style: AxStyle.secondaryTextStyle.copyWith(
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              _skillWidget(title: "Skils Offerd",skills: user.skillsOffered),
-                              const SizedBox(height: 12.0),
-                              _skillWidget(title: "Skils Want",skills: user.skillsOffered),
-                          
-                            ],
-                          ),
-              
+                            ),
+                          if (ratingStar.isNotEmpty)
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5.0),
+                              child: Text(ratingStar),
+                            ),
                         ],
                       ),
-                    ),
-            
-                      
-                  ],
-                
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: AxFilledButton(
-                    label: "Request",
-                    onPressed: () {
-                      naviagteTo(context: context, widget: UserDetails(userModel: user));
-                    }
+                      const SizedBox(width: 16.0),
+                      Flexible(
+                        child: Column(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0,
+                                  ),
+                                  child: Text(
+                                    user.name!,
+                                    style: AxStyle.secondaryTextStyle.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                ),
+                                skillWidget(
+                                  title: "Skils Offerd",
+                                  skills: user.skillsOffered,
+                                ),
+                                const SizedBox(height: 12.0),
+                                skillWidget(
+                                  title: "Skils Want",
+                                  skills: user.skillsOffered,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: AxFilledButton(
+                      label: "Request",
+                      onPressed: () {
+                        naviagteTo(
+                          context: context,
+                          widget: RequestSwap(userModel: user),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        )
+        ),
       );
     }
 
     return children;
   }
 
-  Widget _skillWidget({required title,required List<AxSkillModel> skills}) {
+
+}
+
+  Widget skillWidget({required title, required List<AxSkillModel> skills}) {
     return Wrap(
       spacing: 5.0,
       runSpacing: 3.0,
       runAlignment: WrapAlignment.center,
       children: [
-        Text("$title :",style: TextStyle(color: Colors.green), ),
-        if(skills.isEmpty)
+        Text("$title :", style: TextStyle(color: Colors.green)),
+        if (skills.isEmpty)
           Text("No Skills Availbale")
         else
           ...skills.map((skil) {
             return Container(
               decoration: BoxDecoration(
                 color: Colors.blue.withValues(alpha: 0.6),
-                borderRadius: BorderRadius.circular(12.0)
+                borderRadius: BorderRadius.circular(12.0),
               ),
               padding: const EdgeInsets.symmetric(
                 horizontal: 8.0,
-                vertical: 1.0
+                vertical: 1.0,
               ),
-              child: Text(skil.name!,
-              style: TextStyle(color: AxStyle.white,fontSize: 12.0),),
+              child: Text(
+                skil.name!,
+                style: TextStyle(color: AxStyle.white, fontSize: 12.0),
+              ),
             );
-          })
+          }),
       ],
     );
   }
-}
