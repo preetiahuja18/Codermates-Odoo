5f56afebfcd05a13c9c7f4a061b5910d37ce4de2
Ritesh <riteshsaini331@gmail.com>
2025-07-12 13:08:16 +0530

    s

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:skill_swap/main.dart';
+import 'package:skill_swap/modules/app/view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SkilSwapApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
